cbec1e75a88c6cfb44ce1225fb8a41d0112157d9
vvish <vv.os.swe@gmail.com>
2026-09-07 20:27:01 +0200

    Remove POC for exception support

--- include/cpp_lmdb/error.hpp
+++ include/cpp_lmdb/error.hpp
@@ -3,7 +3,6 @@
 #include "lmdb.h"
 
 // std
-#include <exception>
 #include <expected>
 
 namespace lmdb
@@ -32,70 +31,10 @@ enum class error_t : int {
     bad_dbi = MDB_BAD_DBI,
 };
 
-#ifdef CPP_LMDB_EXCEPTIONS_ENABLED
-
-class lmdb_exception : public std::exception {
-public:
-    lmdb_exception(error_t error) noexcept : _error{error}
-    {}
-
-    auto what() const noexcept -> char const* override
-    {
-        return "";
-    }
-
-    auto error() const noexcept -> error_t
-    {
-        return _error;
-    }
-
-private:
-    error_t _error;
-};
-
-#endif // CPP_LMDB_EXCEPTIONS_ENABLED
-
 }  // namespace lmdb
-
-template <typename T>
-struct extract_parantesized_arg;
-template <typename T, typename A>
-struct extract_parantesized_arg<T(A)> {
-    using arg = A;
-};
-
-#ifdef CPP_LMDB_EXCEPTIONS_ENABLED
-
-#define LMDB_RESULT(res_type) \
-    typename extract_parantesized_arg<void(res_type)>::arg
-#define LMDB_NOEXCEPT
-// #define LMDB_NOEXCEPT_COND(cond) noexcept(cond)
-
-#define LMDB_REPORT_ERROR(code)  \
-    throw ::lmdb::lmdb_exception \
-    {                            \
-        code                     \
-    }
-
-#else
-
-#define LMDB_RESULT(res_type)                                   \
-    std::expected<                                              \
-        typename extract_parantesized_arg<void(res_type)>::arg, \
-        ::lmdb::error_t>
-
-#define LMDB_NOEXCEPT noexcept
-
-#define LMDB_REPORT_ERROR(code) \
-    return std::unexpected      \
-    {                           \
-        code                    \
-    }
-
-#endif
 
 #define LMDB_CALL_API(expr)                                    \
     do {                                                       \
         if (auto const result = (expr); result != MDB_SUCCESS) \
-            LMDB_REPORT_ERROR(::lmdb::error_t{result});        \
+            return std::unexpected{::lmdb::error_t{result}};   \
     } while (false)
